de6097292502fa972f431a60f4ac0c3aadd3a201
cumi <c.karaali16654@hs-ansbach.de>
2021-11-07 02:35:01 +0100

    Übungsblatt-1

---
+++ Übungsblatt-1/src/ErroneousJava.java
@@ -0,0 +1,11 @@
+public class ErroneousJava {
+    public static void main(String[] args) {
+        double x = 3.0;
+        double y = 2.0;
+        double h = 5.0;
+
+        double v = x * y / 3.0 * h;
+
+        System.out.println("v = " + v);
+    }
+}
---
+++ Übungsblatt-1/src/RectCalculator.java
@@ -0,0 +1,15 @@
+public class RectCalculator {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        int u = 2 * a + 2 * b;
+        int f = a * b;
+
+        System.out.print("Der Wert von u lautet: ");
+        System.out.println(u);
+
+        System.out.print("Der Wert von f lautet: ");
+        System.out.println(f);
+    }
+}
